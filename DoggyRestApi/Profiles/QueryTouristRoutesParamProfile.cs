aeae39c0626bdd08e68307058821fc298597bb81
TuanyangQiu <qiutuanyang@gmail.com>
2023-06-24 11:17:26 +1200

    created mapping from QueryTouristRoutesParamDTO to QueryTouristRoutesParam

---
+++ DoggyRestApi/Profiles/QueryTouristRoutesParamProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DoggyRestApi.DTOs;
+using DoggyRestApi.Models;
+
+namespace DoggyRestApi.Profiles
+{
+    public class QueryTouristRoutesParamProfile : Profile
+    {
+        public QueryTouristRoutesParamProfile()
+        {
+            CreateMap<QueryTouristRoutesParamDTO, QueryTouristRoutesParam>();
+        }
+    }
+}
